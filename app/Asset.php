6432abc083d8782597ac57a71ead02847102f1ba
sugarfixx <ingar.torsrud@me.com>
2021-08-18 00:26:06 +0200

    Add Asset Model

---
+++ app/Asset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 18/08/2021
+ * Time: 00:24
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Asset extends Model
+{
+    protected $table = 'asset';
+    
+}
