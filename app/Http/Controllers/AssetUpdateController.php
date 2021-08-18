a7a188571da34966d3e392f07b03fee5e8f60de0
sugarfixx <ingar.torsrud@me.com>
2021-08-18 07:14:11 +0200

    Arred Guzzle and queue

--- app/Http/Controllers/AssetUpdateController.php
+++ app/Http/Controllers/AssetUpdateController.php
@@ -10,19 +10,44 @@ namespace App\Http\Controllers;
 
 
 use App\Asset;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
 
 class AssetUpdateController extends Controller
 {
     protected $companyId =1324004;
+    protected $jwtToken = 'Bearer';
+    protected $mapUrl = 'https://map-api-eu1.mediabank.me/asset/';
 
     // select count(assetid) from asset where assetcompany_id = 1324004;
     public function getCount()
     {
-        $assets = Asset::where('assetcompany_id', $this->companyId)->get();
-        var_dump(count($assets));
+
+        $assets = DB::table('asset')->where('assetcompany_id', $this->companyId)->count();
+        // $assets = Asset::where('assetcompany_id', $this->companyId)->get();
+        var_dump($assets);
     }
+
+    
     public function update()
     {
+        $assets = Asset::where('assetcompany_id', $this->companyId)->get();
 
+    }
+
+    public function callMapApi($assetId)
+    {
+        $client = new Client();
+        $client->put($this->mapUrl, [
+            'headers' => [
+                'Authorization' => $this->jwtToken,
+                'MAP-Application'  => 'library'
+            ],
+            'body' => [
+                'assetid' => $assetId,
+            ],
+            'allow_redirects' => false,
+            'timeout'         => 5
+        ]);
     }
 }
---
+++ app/Queue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 18/08/2021
+ * Time: 07:13
+ */
+
+namespace App;
+
+
+class Queue
+{
+
+}
