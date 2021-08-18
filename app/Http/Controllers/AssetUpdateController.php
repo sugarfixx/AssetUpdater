2188d5be13d0e89ff21a35ba8dfcc35e61437c4a
sugarfixx <ingar.torsrud@me.com>
2021-08-19 00:12:35 +0200

    Implented guzzle exceptions

--- app/Http/Controllers/AssetUpdateController.php
+++ app/Http/Controllers/AssetUpdateController.php
@@ -12,12 +12,15 @@ namespace App\Http\Controllers;
 use App\Asset;
 use App\Queue;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AssetUpdateController extends Controller
 {
     protected $companyId = 1324004;
-    protected $jwtToken = 'Bearer';
+    protected $jwtToken = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtZWRpYWJhbmsubWUiLCJhdWQiOiJtZWRpYWJhbmsubWUiLCJpYXQiOjE2MjkzMjAzMTgsIm5iZiI6MTYyOTMyMDMxOCwiZXhwIjoxNjI5MzIzOTE4LCJ1c2VySWQiOjcxNDEwMDEsInRhZ3NBcGlUb2tlbiI6bnVsbCwiYXBpVG9rZW4iOiJabVUxWmpabE16aGtaREE1TkRnek5HWXhZalJoWTJRME4yVTNZakkxT0RjME5qSXgiLCJ1c2VyIjp7InVzZXJpZCI6NzE0MTAwMSwidXNlcm5hbWUiOiJpdG9yc3J1ZEBuZXBncm91cC5jb20iLCJmdWxsbmFtZSI6IkluZ2FyIFRvcnNydWQgKE1lZGlhYmFuaykiLCJwaG9uZSI6bnVsbCwiZW1haWwiOiJpdG9yc3J1ZEBuZXBncm91cC5jb20iLCJjb21wYW55X2lkIjoiODY0MDA0IiwiY29tcGFueV9uYW1lIjoiREFaTiIsInRhZ3NfYXBpX3Rva2VuIjpudWxsLCJyb2xlcyI6WzEsMTAwMCwxMDA0LDEwMDYsMzUsMzYsMzNdLCJyZWdpb24iOiJldS1oaWx2ZXJzdW0tMSJ9fQ.UeXQWYjS7oD-e02pK-Vaft3c7Qa2QlCRLZQJRNrtf7A';
     protected $mapUrl = 'https://map-api-eu1.mediabank.me/asset/';
 
     // select count(assetid) from asset where assetcompany_id = 1324004;
@@ -36,7 +39,7 @@ class AssetUpdateController extends Controller
 
     public function buildQueue()
     {
-        $assets = Asset::on('pgsql')->where('assetcompany_id', $this->companyId)->limit(2)->pluck('assetid');
+        $assets = Asset::on('pgsql')->where('assetcompany_id', $this->companyId)->get()->pluck('assetid');
         $processed = 0;
         foreach ($assets as $asset) {
             $message = json_encode(['assetId' => $asset]);
@@ -59,29 +62,50 @@ class AssetUpdateController extends Controller
 
     public function runQueue()
     {
-        $queue = Queue::where('done', 'false')->take(1)->get();
-
+        $queue = Queue::where('done', 'false')->take(500)->get();
+        $i = 0;
         foreach ($queue as $entry)  {
+            $i++;
             $item = json_decode($entry->item);
             $assetId = $item->assetId;
+            if ($this->callMapApi($assetId) !== false) {
+                $message = 'Success: Asset ' . $assetId .' updated through map api';
+            } else {
+                $message = 'Error: Asset ' . $assetId .' failed to be updated through map api';
+            }
+            Log::info($message);
         }
-
+        return response()->json(['message' => $i . ' items processed']);
     }
 
     public function callMapApi($assetId)
     {
         $client = new Client();
-        $client->put($this->mapUrl, [
+        $request = [
             'headers' => [
                 'Authorization' => $this->jwtToken,
                 'MAP-Application'  => 'library'
             ],
-            'body' => [
-                'assetid' => $assetId,
-            ],
-            'allow_redirects' => false,
-            'timeout'         => 5
-        ]);
-        
+            'form_params' => [
+                'id' => $assetId,
+                'assetmeta' => json_encode(['updatedBy' => 'AssetUpdater']),
+                'notify' => 0
+            ]
+        ];
+
+        try {
+            $response = $client->request('PUT', $this->mapUrl, $request);
+            if ($response->getStatusCode()== 200 ) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (ClientException $e) {
+            Log::info($e->getMessage());
+        } catch (ServerException $e ) {
+            Log::info($e->getMessage());
+        } catch (\Exception $e) {
+            Log::info($e->getMessage());
+        }
     }
 }
