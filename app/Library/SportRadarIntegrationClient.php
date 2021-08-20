e36d342197738f15825b160b4997fbfe7d0ce227
sugarfixx <ingar.torsrud@me.com>
2021-08-20 14:47:27 +0200

    Update Sportradar client
    Signed-off-by: sugarfixx <ingar.torsrud@me.com>

--- app/Library/SportRadarIntegrationClient.php
+++ app/Library/SportRadarIntegrationClient.php
@@ -8,8 +8,42 @@
 
 namespace App\Library;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use Illuminate\Support\Facades\Log;
 
 class SportRadarIntegrationClient
 {
+    public function getMetadata($assetMeta)
+    {
+        $client = new Client(['base_uri' => $this->baseUrl]);
+        $request = [
+            'headers' => [
+                'accept' => 'accept: application/json',
+                'Content-Type'=> 'application/json'
+            ],
+            'json' => [
+                json_encode($assetMeta)
+            ]
+        ];
+        try {
+            $response = $client->request('POST', 'login', $request);
+            if ($response->getStatusCode()== 200 ) {
+                $body = $response->getBody();
+                if ($body)  {
+                    return $body;
+                }
 
+            } else {
+                return false;
+            }
+        } catch (ClientException $e) {
+            Log::info($e->getMessage());
+        } catch (ServerException $e ) {
+            Log::info($e->getMessage());
+        } catch (\Exception $e) {
+            Log::info($e->getMessage());
+        }
+    }
 }
